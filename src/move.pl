53d583241bc0cb98ad152f530b45207470cac7b0
Thiago Paiva <thiagohdaqw@gmail.com>
2022-07-27 00:24:51 -0300

    Refatorando leitura dos movimentos

--- src/move.pl
+++ src/move.pl
@@ -3,15 +3,13 @@ readPlayerMove(Turn, Move) :-
     format('\nJogador[~d]: Insira sua jogada:\n', Turn+1), flush_output(),
     read(MoveReaded),
     name(MoveReaded,MoveList),
-    checkMove(Turn, MoveList, Move).
-
-checkMove(Turn, MoveList, Move) :-
-    not(isMoveFormatValid(MoveList)), !,
+    isMoveFormatValid(MoveList), !,
+    parseMove(MoveList, Move).
+readPlayerMove(Turn, Move) :-
     write('\nFormato de mensagem invalido!\n'),
     write('Informe a posicao atual e pra onde quer ir no seguinte formato: OrigemDestino!\n'),
     write('Exemplo: e2f5\n'), flush_output(),
     readPlayerMove(Turn, Move).
-checkMove(_, MoveList, Move) :- parseMove(MoveList, Move).
 
 
 parseMove([A,B,C,D], Move) :-
