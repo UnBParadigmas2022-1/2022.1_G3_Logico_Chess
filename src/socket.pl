d2e7c2428d49e9d16921bce75d7bd37d7a48b2d7
Thiago Paiva <thiagohdaqw@gmail.com>
2022-07-26 22:32:26 -0300

    Adicionando regra para fechar conexoes com os clients

--- src/socket.pl
+++ src/socket.pl
@@ -1,10 +1,11 @@
 :- use_module(socket).
 
-:- dynamic(clients/3).
+:- dynamic(clients/4).
 
 
 createServer(Port, AcceptFd) :- 
     tcp_socket(Socket),
+    tcp_setopt(Socket, reuseaddr),
     tcp_bind(Socket, Port),
     tcp_listen(Socket, 2),
     tcp_open_socket(Socket, AcceptFd, _).
@@ -12,6 +13,14 @@ createServer(Port, AcceptFd) :-
 
 acceptClient(AcceptFd, Turn) :-
     format('Player[~d]: Aguardando conexão\n', Turn+1),
-    tcp_accept(AcceptFd, Socket, Peer),
+    tcp_accept(AcceptFd, Socket, _),
+    tcp_open_socket(Socket, StreamIn, StreamOut),
     format('Player[~d]: Conectado\n', Turn+1),
-    assert(clients(Turn, Socket, Peer)).
+    assert(clients(Turn, Socket, StreamIn, StreamOut)).
+
+
+closeClients() :- findall(S, clients(_,S,_,_), L), closeClient(L).
+
+closeClient([]) :- !.
+closeClient([S]) :- tcp_close_socket(S).
+closeClient([H|T]) :- closeClient([H]), closeClient(T).
