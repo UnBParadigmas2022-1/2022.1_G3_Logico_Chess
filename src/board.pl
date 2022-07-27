a76070b324ca11bc6cb21dee8a90d2af21d97263
RcleydsonR <rafael.cleydson@gmail.com>
2022-07-27 17:24:08 -0300

    Posição inicial do peão

---
+++ src/board.pl
@@ -0,0 +1,17 @@
+% Initial pawn
+board(0, 1, pawn, white). %% a2
+board(0, 6, pawn, black). %% a7
+board(1, 1, pawn, white). %% b2
+board(1, 6, pawn, black). %% b7
+board(2, 1, pawn, white). %% c2
+board(2, 6, pawn, black). %% c7
+board(3, 1, pawn, white). %% d2
+board(3, 6, pawn, black). %% d7
+board(4, 1, pawn, white). %% e2
+board(4, 6, pawn, black). %% e7
+board(5, 1, pawn, white). %% f2
+board(5, 6, pawn, black). %% f7
+board(6, 1, pawn, white). %% g2
+board(6, 6, pawn, black). %% g7
+board(7, 1, pawn, white). %% h2
+board(7, 6, pawn, black). %% h7
