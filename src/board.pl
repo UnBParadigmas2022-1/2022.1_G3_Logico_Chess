0476c1835b729b61b867238513536fca973fe2b6
RcleydsonR <rafael.cleydson@gmail.com>
2022-07-27 17:28:25 -0300

    Posição inicial dos bispos, cavalos e torres

--- src/board.pl
+++ src/board.pl
@@ -15,3 +15,21 @@ board(6, 1, pawn, white). %% g2
 board(6, 6, pawn, black). %% g7
 board(7, 1, pawn, white). %% h2
 board(7, 6, pawn, black). %% h7
+
+% Initial Bishop
+board(2, 0, bishop, white). %% c1
+board(2, 7, bishop, black). %% f1
+board(5, 0, bishop, white). %% c8
+board(5, 7, bishop, black). %% f8
+
+% Initial knight
+board(1, 0, knight, white). %% b1
+board(6, 0, knight, black). %% g1
+board(1, 7, knight, white). %% b8
+board(6, 7, knight, black). %% g8
+
+% Initial Rook
+board(0, 0, rook, white). %% a1
+board(7, 0, rook, black). %% a8
+board(0, 7, rook, white). %% h1
+board(7, 7, rook, black). %% h8
