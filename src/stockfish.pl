e450f496c4db3fee25178cefab5d538ad24b4ba4
Joao-Moura <jaummoura15@gmail.com>
2022-07-28 00:02:57 -0300

    Adicionando maneira de enviar comandos e ler o output do stockfish

--- src/stockfish.pl
+++ src/stockfish.pl
@@ -1,3 +1,20 @@
 % start_stockfish/2 -> Cria pipes stdin e stdout para comunicação com o stockfish
 start_stockfish(Stockfish, Out) :-
     process_create(path(stockfish), [], [stdin(pipe(Stockfish)), stdout(pipe(Out))]).
+
+% send_command/2 -> Envia comando SEM retorno de terminal (stdin não é fechada)
+send_command(Stockfish, Command) :-
+    writeln(Stockfish, Command),
+    flush_output(Stockfish).
+
+% read_output/2 -> Prepara leitura de stdout com lista de códigos
+% read_output/3 -> Converte valor para letra e salva na lista
+read_output(Out, Lines) :-
+    read_line_to_codes(Out, Codes),
+    read_output(Out, Codes, Lines).
+
+read_output(_, end_of_file, []) :- !.
+read_output(Out, Codes, [Line|Lines]) :-
+    atom_codes(Line, Codes),
+    read_line_to_codes(Out, Next),
+    read_output(Out, Next, Lines).
