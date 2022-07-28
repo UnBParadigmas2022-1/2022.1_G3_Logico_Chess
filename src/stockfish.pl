ecf181cc2f9c3de15107237cb09d8075786672f5
Joao-Moura <jaummoura15@gmail.com>
2022-07-28 00:05:59 -0300

    Adicionando comando para achar a melhor jogada e sobrescrita do send_command/4

--- src/stockfish.pl
+++ src/stockfish.pl
@@ -3,9 +3,15 @@ start_stockfish(Stockfish, Out) :-
     process_create(path(stockfish), [], [stdin(pipe(Stockfish)), stdout(pipe(Out))]).
 
 % send_command/2 -> Envia comando SEM retorno de terminal (stdin não é fechada)
+% send_command/4 -> Envia comando COM retorno de terminal (nesse caso é preciso fechar o stdin)
 send_command(Stockfish, Command) :-
     writeln(Stockfish, Command),
     flush_output(Stockfish).
+
+send_command(Stockfish, Out, Command, Lines) :-
+    send_command(Stockfish, Command),
+    close(Stockfish),
+    read_output(Out, Lines).
 
 % read_output/2 -> Prepara leitura de stdout com lista de códigos
 % read_output/3 -> Converte valor para letra e salva na lista
@@ -23,3 +29,12 @@ read_output(Out, Codes, [Line|Lines]) :-
 set_fen_position(Stockfish, Fen) :-
     atomics_to_string(['position', 'fen', Fen], " ", Command),
     send_command(Stockfish, Command).
+
+% get_best_move/5 -> Comando que acha o melhor movimento, de acordo com
+% a posição Fen passada e a profundidade Depth de procura
+get_best_move(Stockfish, Out, Fen, Depth, Move) :-
+    set_fen_position(Stockfish, Fen),
+    atomics_to_string(['go', 'depth', Depth], " ", Command),
+    send_command(Stockfish, Out, Command, Lines),
+    last(Lines, Line),
+    split_string(Line, " ", "", Last), last(Last, Move).
