d7ad6c060df367fb849f08611684ba5d2924acd3
Antonio Aldisio <aldisiofilho@gmail.com>
2022-07-28 21:00:59 -0300

    movimento para atras

--- src/pieces/rook.pl
+++ src/pieces/rook.pl
@@ -1,12 +1,14 @@
 :-consult(board).
 
 isRookMoveValid(Turn, Move):-
-    (isVerticalEmpty(Move); isHorizontalEmpty(Move)),
+    (isVerticalEmpty(Move); isHorizontalEmpty(Move);
+    isVerticalBackEmpty(Move); isHorizontalBackEmpty(Move)),
     verifyCollision(Move),
     isSimpleMoveValid(Turn, Move).
 
 
 verifyCollision([_, _, Cx, Cy]):- not(board(Cx, Cy, _, _)).
+
 
 isVerticalEmpty([Px, Py, Cx, Cy]):-
     Px =:= Cx,
@@ -17,6 +19,17 @@ isVerticalEmpty([Px, Py, Cx, Cy]):-
 isVerticalEmpty([Px, X1, Cx, Cy]):-
     write('Caminho limpo'),nl.
 
+
+isVerticalBackEmpty([Px, Py, Cx, Cy]):-
+    Px =:= Cx,
+    Py =\= Cy, !,
+    decrement(Py, X1, 1),
+    not(board(Cx, X1, _, _)),
+    isVerticalEmpty([Px, X1, Cx, Cy]).
+isVerticalEmpty([Px, X1, Cx, Cy]):-
+    write('Caminho limpo'),nl.
+
+
 isHorizontalEmpty([Px, Py, Cx, Cy]):-
     Px =:= Cx,
     Py =\= Cy, !,
@@ -26,17 +39,28 @@ isHorizontalEmpty([Px, Py, Cx, Cy]):-
 isHorizontalEmpty([Px, X1, Cx, Cy]):-
     write('Caminho limpo'),nl.
 
+
+isHorizontalBackEmpty([Px, Py, Cx, Cy]):-
+    Px =:= Cx,
+    Py =\= Cy, !,
+    decrement(Py, X1, 1),
+    not(board(Cx, X1, _, _)),
+    isHorizontalEmpty([Px, X1, Cx, Cy]).
+isHorizontalEmpty([Px, X1, Cx, Cy]):-
+    write('Caminho limpo'),nl.
+
+
 isSimpleMoveValid(white, [Px, Py, Cx, Cy]):-
-    format('\nPx[~r]:\n', Px), flush_output(),
-    format('\nPy[~r]:\n', Py), flush_output(),
-    format('\nCx[~r]:\n', Cx), flush_output(),
-    format('\nCy[~r]:\n', Cy), flush_output().
+    write(' '),nl.
+
 
 isSimpleMoveValid(black, [Px, Py, Cx, Cy]):-
-    format('\nPx[~r]:\n', Px), flush_output(),
-    format('\nPy[~r]:\n', Py), flush_output(),
-    format('\nCx[~r]:\n', Cx), flush_output(),
-    format('\nCy[~r]:\n', Cy), flush_output().
+    write(' '),nl.
+
 
 increment(X, X1):-
     X1 is X+1.
+
+
+decrement(X, X1):-
+    X1 is X-1.
