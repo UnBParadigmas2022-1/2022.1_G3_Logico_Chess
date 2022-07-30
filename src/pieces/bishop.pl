8bc7494ccf4db6457b14d91857ba03d839032a91
RcleydsonR <rafael.cleydson@gmail.com>
2022-07-30 14:10:41 -0300

    Correção na comilança do bispo
    Co-authored-by: Thiago Paiva <thiagohdaqw@gmail.com>
    Co-authored-by: Dafne Moretti <moretti.dafne2@gmail.com>

--- src/pieces/bishop.pl
+++ src/pieces/bishop.pl
@@ -1,20 +1,20 @@
 :-consult(board).
 
-isBishopMoveValid(Turn, [Px, Py, Cx, Cy]):- Px == Cx, Py == Cy, fail.
-isBishopMoveValid(Turn, [Px, Py, Cx, Cy]):- board(Cx, Cy, _, Turn), fail. 
-isBishopMoveValid(Turn, [Px, Py, Cx, Cy]):- abs(Px-Cx) =\= abs(Py-Cy), fail. % so deixa andar na diagonal
-isBishopMoveValid(Turn, Move):- 
+isBishopMoveValid(_, [Px, Py, Px, Py]):- fail.
+isBishopMoveValid(Turn, [_, _, Cx, Cy]):- board(Cx, Cy, _, Turn, _), !, fail. 
+isBishopMoveValid(_, [Px, Py, Cx, Cy]):- abs(Px-Cx) =\= abs(Py-Cy), fail. % so deixa andar na diagonal
+isBishopMoveValid(_, Move):- 
     isFreeWay(Move, 1, 1);
     isFreeWay(Move, 1, -1);
     isFreeWay(Move, -1, 1);
     isFreeWay(Move, -1, -1).
 
-isFreeWay([Px, Py, Cx, Cy], StepX, StepY) :- (Px > 7; Px < 0; Py > 7; Py < 0), !, fail. % condicao de parada
+isFreeWay([Px, Py, _, _], _, _) :- (Px > 7; Px < 0; Py > 7; Py < 0), !, fail. % condicao de parada
 isFreeWay([Px, Py, Cx, Cy], StepX, StepY) :-
     X is Px+StepX,
     Y is Py+StepY,
     X == Cx, Y == Cy;
         XX is Px+StepX,
         YY is Py+StepY,
-        not(board(XX, YY,_,_)),
+        not(board(XX, YY,_,_,_)),
         isFreeWay([XX, YY, Cx, Cy], StepX, StepY).
