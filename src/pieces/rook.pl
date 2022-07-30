b4ae5ea9f55a171ce5cbcd8c2e0f742e6179e37c
Antonio Aldisio <aldisiofilho@gmail.com>
2022-07-29 21:33:45 -0300

    refatoracao dos movimento atras e frente

--- src/pieces/rook.pl
+++ src/pieces/rook.pl
@@ -1,66 +1,77 @@
 :-consult(board).
 
 isRookMoveValid(Turn, Move):-
-    (isVerticalEmpty(Move); isHorizontalEmpty(Move);
-    isVerticalBackEmpty(Move); isHorizontalBackEmpty(Move)),
-    verifyCollision(Move),
-    isSimpleMoveValid(Turn, Move).
+    verificaVerticalFront(Move);
+    verificaVerticalDown(Move);
+    verificaHorizontalRight(Move);
+    verificaHorizontalLeft(Move).
 
 
-verifyCollision([_, _, Cx, Cy]):- not(board(Cx, Cy, _, _)).
+verificaVerticalFront([Px, Py, Cx, Cy]):-
+    Px == Cx,
+    Py < Cy,
+    write('Sou Vertical Front'),nl,
+    isVerticalFrontEmpty([Px, Py, Cx, Cy]).
+
+verificaVerticalDown([Px, Py, Cx, Cy]):-
+    Px == Cx,
+    Py > Cy,
+    write('Sou Vertical Down'),nl,
+    isVerticalDownEmpty([Px, Py, Cx, Cy]).
+
+isVerticalFrontEmpty([Px, Py, Cx, Cy]):-
+    increment(Py, X1),
+    X1 =\= Cy+1, !,
+    not(board(Px, X1, _, _)),
+    isVerticalFrontEmpty([Px, X1, Cx, Cy]).
+isVerticalFrontEmpty([Px, X1, Cx, Cy]):-
+    Px == Cx,
+    X1 == Cy.
+
+isVerticalDownEmpty([Px, Py, Cx, Cy]):-
+    decrement(Py, X1),
+    X1 =\= Cy-1, !,
+    not(board(Px, X1, _, _)),
+    isVerticalDownEmpty([Px, X1, Cx, Cy]).
+isVerticalDownEmpty([Px, X1, Cx, Cy]):-
+    Px == Cx,
+    X1 == Cy.
 
 
-isVerticalEmpty([Px, Py, Cx, Cy]):-
-    Px =:= Cx,
-    Py =\= Cy, !,
-    increment(Py, X1, 1),
-    not(board(Cx, X1, _, _)),
-    isVerticalEmpty([Px, X1, Cx, Cy]).
-isVerticalEmpty([Px, X1, Cx, Cy]):-
-    write('Caminho limpo'),nl.
+verificaHorizontalRight([Px, Py, Cx, Cy]):-
+    Py == Cy,
+    Px < Cx,
+    write('Sou Hozizontal Right'),nl,
+    isHorizontalRightEmpty([Px, Py, Cx, Cy]).
 
+isHorizontalRightEmpty([Px, Py, Cx, Cy]):-
+    increment(Px, X1),
+    X1 =\= Cx+1, !,
+    not(board(X1,Py, _, _)),
+    isHorizontalRightEmpty([X1, Py, Cx, Cy]).
+isHorizontalRightEmpty([X1, Py, Cx, Cy]):-
+    X1 == Cx,
+    Py == Cy.
 
-isVerticalBackEmpty([Px, Py, Cx, Cy]):-
-    Px =:= Cx,
-    Py =\= Cy, !,
-    decrement(Py, X1, 1),
-    not(board(Cx, X1, _, _)),
-    isVerticalEmpty([Px, X1, Cx, Cy]).
-isVerticalEmpty([Px, X1, Cx, Cy]):-
-    write('Caminho limpo'),nl.
+verificaHorizontalLeft([Px, Py, Cx, Cy]):-
+    Py == Cy,
+    Px > Cx,
+    write('Sou Hozizontal Left'),nl,
+    isHorizontalLeftEmpty([Px, Py, Cx, Cy]).
 
-
-isHorizontalEmpty([Px, Py, Cx, Cy]):-
-    Px =:= Cx,
-    Py =\= Cy, !,
-    increment(Py, X1, 1),
-    not(board(Cx, X1, _, _)),
-    isHorizontalEmpty([Px, X1, Cx, Cy]).
-isHorizontalEmpty([Px, X1, Cx, Cy]):-
-    write('Caminho limpo'),nl.
-
-
-isHorizontalBackEmpty([Px, Py, Cx, Cy]):-
-    Px =:= Cx,
-    Py =\= Cy, !,
-    decrement(Py, X1, 1),
-    not(board(Cx, X1, _, _)),
-    isHorizontalEmpty([Px, X1, Cx, Cy]).
-isHorizontalEmpty([Px, X1, Cx, Cy]):-
-    write('Caminho limpo'),nl.
-
-
-isSimpleMoveValid(white, [Px, Py, Cx, Cy]):-
-    write(' '),nl.
-
-
-isSimpleMoveValid(black, [Px, Py, Cx, Cy]):-
-    write(' '),nl.
+isHorizontalLeftEmpty([Px, Py, Cx, Cy]):-
+    decrement(Px, X1),
+    X1 =\= Cx-1, !,
+    not(board(X1,Py, _, _)),
+    isHorizontalLeftEmpty([X1, Py, Cx, Cy]).
+isHorizontalLeftEmpty([X1, Py, Cx, Cy]):-
+    X1 == Cx,
+    Py == Cy.
 
 
 increment(X, X1):-
     X1 is X+1.
 
-
 decrement(X, X1):-
     X1 is X-1.
+
