b6d5ef0b60876723906836542b48a389e1e59b5b
DafneM <moretti.dafne2@gmail.com>
2022-07-30 15:49:44 -0300

    Adiciona validacao de FreeWay na queen
    Co-authored-by: Rafael Cleydson <rafael.cleydson@gmail.com>

--- src/pieces/queen.pl
+++ src/pieces/queen.pl
@@ -2,3 +2,12 @@
 
 isQueenMoveValid(_, [Px, Py, Px, Py]):- fail.
 isQueenMoveValid(Turn, [_, _, Cx, Cy]):- board(Cx, Cy, _, Turn, _), !, fail. 
+isQueenMoveValid(_, Move):- 
+    isFreeWay(Move, 1, 1);
+    isFreeWay(Move, 1, -1);
+    isFreeWay(Move, -1, 1);
+    isFreeWay(Move, -1, -1);
+    isFreeWay(Move, 1, 0);
+    isFreeWay(Move, -1, 0);
+    isFreeWay(Move, 0, 1);
+    isFreeWay(Move, 0, -1).
