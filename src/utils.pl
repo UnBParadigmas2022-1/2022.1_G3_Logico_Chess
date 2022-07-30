2c78b22d6b2dd0131cdefb7b32af97ffd6416168
DafneM <moretti.dafne2@gmail.com>
2022-07-30 15:46:44 -0300

    Adiciona funcao FreeWay no utils
    Co-authored-by: Rafael Cleydson <rafael.cleydson@gmail.com>

--- src/utils.pl
+++ src/utils.pl
@@ -3,3 +3,13 @@ increment(X, X1, Qty):-
 
 decrement(X, X1, Qty):-
     X1 is X-Qty.
+
+isFreeWay([Px, Py, _, _], _, _) :- (Px > 7; Px < 0; Py > 7; Py < 0), !, fail. % condicao de parada
+isFreeWay([Px, Py, Cx, Cy], StepX, StepY) :-
+    increment(Px, X, StepX),
+    increment(Py, Y, StepY),
+    X == Cx, Y == Cy;
+        increment(Px, XX, StepX),
+        increment(Py, YY, StepY),
+        not(board(XX, YY,_,_,_)),
+        isFreeWay([XX, YY, Cx, Cy], StepX, StepY).
