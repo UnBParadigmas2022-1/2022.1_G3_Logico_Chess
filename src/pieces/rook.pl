d93dad7c397002b7b609693a5534f9c21aae283c
Antonio Aldisio <aldisiofilho@gmail.com>
2022-07-29 22:33:48 -0300

    inclusao do ataque da torre

--- src/pieces/rook.pl
+++ src/pieces/rook.pl
@@ -1,70 +1,81 @@
 :-consult(board).
 
 isRookMoveValid(Turn, Move):-
-    verificaVerticalFront(Move);
-    verificaVerticalDown(Move);
-    verificaHorizontalRight(Move);
-    verificaHorizontalLeft(Move).
+    verificaVerticalFront(Turn, Move);
+    verificaVerticalDown(Turn, Move);
+    verificaHorizontalRight(Turn, Move);
+    verificaHorizontalLeft(Turn, Move).
 
 
-verificaVerticalFront([Px, Py, Cx, Cy]):-
+
+atack(Turn, [Px, Py, Cx, Cy]):-
+    Turn == white, !,
+    board(Px, Py, _, black).
+atack(Turn, [Px, Py, Cx, Cy]):-
+    Turn == black,
+    board(Px, Py, _, white).
+
+% Feito atack para o caso de o jogador selecionar uma peça do adversário.
+verificaVerticalFront(Turn, [Px, Py, Cx, Cy]):-
     Px == Cx,
     Py < Cy,
     write('Sou Vertical Front'),nl,
-    isVerticalFrontEmpty([Px, Py, Cx, Cy]).
+    isVerticalFrontEmpty(Turn, [Px, Py, Cx, Cy]).
+% Feito atack para o caso de o jogador selecionar uma peça do adversário.
+isVerticalFrontEmpty(Turn, [Px, Py, Cx, Cy]):-
+    increment(Py, X1),
+    X1 =\= Cy+1, !,
+    (atack(Turn, [Px, X1, Cx, Cy]) ; not(board(Px, X1, _, _))),
+    isVerticalFrontEmpty(Turn, [Px, X1, Cx, Cy]).
+isVerticalFrontEmpty(Turn, [Px, X1, Cx, Cy]):-
+    Px == Cx,
+    X1 == Cy.
 
-verificaVerticalDown([Px, Py, Cx, Cy]):-
+% Feito atack para o caso de o jogador selecionar uma peça do adversário.
+verificaVerticalDown(Turn, [Px, Py, Cx, Cy]):-
     Px == Cx,
     Py > Cy,
     write('Sou Vertical Down'),nl,
-    isVerticalDownEmpty([Px, Py, Cx, Cy]).
+    isVerticalDownEmpty(Turn, [Px, Py, Cx, Cy]).
 
-isVerticalFrontEmpty([Px, Py, Cx, Cy]):-
-    increment(Py, X1),
-    X1 =\= Cy+1, !,
-    not(board(Px, X1, _, _)),
-    isVerticalFrontEmpty([Px, X1, Cx, Cy]).
-isVerticalFrontEmpty([Px, X1, Cx, Cy]):-
-    Px == Cx,
-    X1 == Cy.
-
-isVerticalDownEmpty([Px, Py, Cx, Cy]):-
+% Feito atack para o caso de o jogador selecionar uma peça do adversário.
+isVerticalDownEmpty(Turn, [Px, Py, Cx, Cy]):-
     decrement(Py, X1),
     X1 =\= Cy-1, !,
-    not(board(Px, X1, _, _)),
-    isVerticalDownEmpty([Px, X1, Cx, Cy]).
-isVerticalDownEmpty([Px, X1, Cx, Cy]):-
+    (atack(Turn, [Px, X1, Cx, Cy]) ; not(board(Px, X1, _, _))),
+    isVerticalDownEmpty(Turn, [Px, X1, Cx, Cy]).
+isVerticalDownEmpty(Turn, [Px, X1, Cx, Cy]):-
     Px == Cx,
     X1 == Cy.
 
 
-verificaHorizontalRight([Px, Py, Cx, Cy]):-
+verificaHorizontalRight(Turn, [Px, Py, Cx, Cy]):-
     Py == Cy,
     Px < Cx,
     write('Sou Hozizontal Right'),nl,
-    isHorizontalRightEmpty([Px, Py, Cx, Cy]).
+    isHorizontalRightEmpty(Turn, [Px, Py, Cx, Cy]).
 
-isHorizontalRightEmpty([Px, Py, Cx, Cy]):-
+isHorizontalRightEmpty(Turn, [Px, Py, Cx, Cy]):-
     increment(Px, X1),
     X1 =\= Cx+1, !,
-    not(board(X1,Py, _, _)),
-    isHorizontalRightEmpty([X1, Py, Cx, Cy]).
-isHorizontalRightEmpty([X1, Py, Cx, Cy]):-
+    (atack(Turn, [X1, Py, Cx, Cy]) ; not(board(X1,Py, _, _))),
+    isHorizontalRightEmpty(Turn, [X1, Py, Cx, Cy]).
+isHorizontalRightEmpty(Turn, [X1, Py, Cx, Cy]):-
     X1 == Cx,
     Py == Cy.
 
-verificaHorizontalLeft([Px, Py, Cx, Cy]):-
+verificaHorizontalLeft(Turn, [Px, Py, Cx, Cy]):-
     Py == Cy,
     Px > Cx,
     write('Sou Hozizontal Left'),nl,
-    isHorizontalLeftEmpty([Px, Py, Cx, Cy]).
+    isHorizontalLeftEmpty(Turn, [Px, Py, Cx, Cy]).
 
-isHorizontalLeftEmpty([Px, Py, Cx, Cy]):-
+isHorizontalLeftEmpty(Turn, [Px, Py, Cx, Cy]):-
     decrement(Px, X1),
     X1 =\= Cx-1, !,
-    not(board(X1,Py, _, _)),
-    isHorizontalLeftEmpty([X1, Py, Cx, Cy]).
-isHorizontalLeftEmpty([X1, Py, Cx, Cy]):-
+    (atack(Turn, [X1, Py, Cx, Cy]) ; not(board(X1,Py, _, _))),
+    isHorizontalLeftEmpty(Turn, [X1, Py, Cx, Cy]).
+isHorizontalLeftEmpty(Turn, [X1, Py, Cx, Cy]):-
     X1 == Cx,
     Py == Cy.
 
