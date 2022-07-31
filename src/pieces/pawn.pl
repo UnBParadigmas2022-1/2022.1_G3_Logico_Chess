f9edc5b5f32bcd867c9a956febe37c8809a7d15f
igorq937 <igorq937@gmail.com>
2022-07-31 18:29:40 -0300

    Adicionado movimento en passant

--- src/pieces/pawn.pl
+++ src/pieces/pawn.pl
@@ -1,9 +1,15 @@
+:- dynamic enPassantBlack/2.
+:- dynamic enPassantWhite/2.
+
+
 isPawnMoveValid(Turn, Move):-
     (verifyPawnAttack(Turn, Move);
+    verifyEnPassantAttack(Turn, Move);
     (verifyCollision(Move),
     (verifyFirstMove(Turn, Move);
     isSimpleMoveValid(Turn, Move)))),
-    applyPromotion(Turn, Move).
+    applyPromotion(Turn, Move),
+    verifyEnPassantMove(Turn, Move).
 
 
 verifyCollision([_, _, Cx, Cy]):- not(board(Cx, Cy, _, _, _)).
@@ -49,3 +55,35 @@ applyPromotion(_, [X, Y, _, Cy]) :-
     (Cy =:= 0 ; Cy =:= 7),
     changePiece(X, Y, queen).
 applyPromotion(_, _).
+
+
+verifyEnPassantMove(black, [_, Py, Cx, Cy]) :-
+    Py - Cy =:= 2,
+    Cy =:= 4,
+    increment(Cx, X1, 1), decrement(Cx, X2, 1),
+    assert(enPassantWhite(X1, Cy)),
+    assert(enPassantWhite(X2, Cy)).
+verifyEnPassantMove(white, [_, Py, Cx, Cy]) :-
+    Cy - Py =:= 2,
+    Cy =:= 3,
+    increment(Cx, X1, 1), decrement(Cx, X2, 1),
+    assert(enPassantBlack(X1, Cy)),
+    assert(enPassantBlack(X2, Cy)).
+verifyEnPassantMove(white, _) :-
+    retractall(enPassantWhite(_, _)).
+verifyEnPassantMove(black, _) :-
+    retractall(enPassantBlack(_, _)).
+
+
+verifyEnPassantAttack(white, [Px, Py, Cx, Cy]) :-
+    enPassantWhite(Px, Py),
+    (Cx =:= Px + 1; Cx =:= Px - 1),
+    (Cy =:= Py + 1),
+    decrement(Cy, Cy1, 1),
+    board(Cx, Cy1, pawn, black, _).
+verifyEnPassantAttack(black, [Px, Py, Cx, Cy]) :-
+    enPassantBlack(Px, Py),
+    (Cx =:= Px + 1; Cx =:= Px - 1),
+    (Cy =:= Py - 1),
+    increment(Cy, Cy1, 1),
+    board(Cx, Cy1, pawn, white, _).
