5d3ebd97650aa0b1b9ad8a5c6875eb9081cd8388
DafneM <moretti.dafne2@gmail.com>
2022-07-30 21:05:24 -0300

    Refatora codigo do rei para mais simplificado

--- src/pieces/king.pl
+++ src/pieces/king.pl
@@ -2,20 +2,6 @@
 
 isKingMoveValid(_, [Px, Py, Px, Py]):- fail.
 isKingMoveValid(Turn, [_, _, Cx, Cy]):- board(Cx, Cy, _, Turn, _), !, fail. 
-isKingMoveValid(_, Move):- 
-    isKingFreeWay(Move, 1, 1);
-    isKingFreeWay(Move, 1, -1);
-    isKingFreeWay(Move, -1, 1);
-    isKingFreeWay(Move, -1, -1);
-    isKingFreeWay(Move, 1, 0);
-    isKingFreeWay(Move, -1, 0);
-    isKingFreeWay(Move, 0, 1);
-    isKingFreeWay(Move, 0, -1).
-
-isKingFreeWay([Px, Py, Cx, Cy], StepX, StepY) :-
-    increment(Px, X, StepX),
-    increment(Py, Y, StepY),
-    X == Cx, Y == Cy;
-        increment(Px, XX, StepX),
-        increment(Py, YY, StepY),
-        not(board(XX, YY,_,_,_)).
+isKingMoveValid(Turn, [Px, Py, Cx, Cy]):- 
+  abs(Px-Cx) =< 1,
+  abs(Py-Cy) =< 1.
