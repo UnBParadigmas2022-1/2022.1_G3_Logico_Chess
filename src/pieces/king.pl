6fbc51f2b97e8754d9254894e95f4f53683252c6
DafneM <moretti.dafne2@gmail.com>
2022-07-30 21:29:00 -0300

    Adicionando Turn como variavel anonima

--- src/pieces/king.pl
+++ src/pieces/king.pl
@@ -2,6 +2,6 @@
 
 isKingMoveValid(_, [Px, Py, Px, Py]):- fail.
 isKingMoveValid(Turn, [_, _, Cx, Cy]):- board(Cx, Cy, _, Turn, _), !, fail. 
-isKingMoveValid(Turn, [Px, Py, Cx, Cy]):- 
+isKingMoveValid(_, [Px, Py, Cx, Cy]):- 
   abs(Px-Cx) =< 1,
   abs(Py-Cy) =< 1.
